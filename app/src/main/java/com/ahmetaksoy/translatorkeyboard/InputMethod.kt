a063b80a9358da7b78fa322f8404f4460402a1e6
ahmetaksoyy <ahmettaksoyy00@gmail.com>
2023-03-02 22:40:59 +0300

    new commit
    -UI uptaded
    -textView added

--- app/src/main/java/com/ahmetaksoy/translatorkeyboard/InputMethod.kt
+++ app/src/main/java/com/ahmetaksoy/translatorkeyboard/InputMethod.kt
@@ -6,22 +6,60 @@ import android.inputmethodservice.KeyboardView
 import android.text.TextUtils
 import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.InputConnection
+import android.widget.ImageView
+import android.widget.TextView
 
 class InputMethod:InputMethodService(),KeyboardView.OnKeyboardActionListener {
     private var keyboardView: KeyboardView? = null
     private var keyboard: Keyboard? = null
     private var caps = false
+    private lateinit var textView: TextView
+    private var kelime : String =""
+     lateinit var enterImage: ImageView
+    lateinit var inputConnection: InputConnection
 
+    //klavyenin ust tarafındaki textviewin enterini kullanabilmek için.
+    override fun onFinishInputView(finishingInput: Boolean) {
+        super.onFinishInputView(finishingInput)
+        enterImage.setOnClickListener{
+            inputConnection.sendKeyEvent(
+                KeyEvent(
+                    KeyEvent.ACTION_DOWN,
+                    KeyEvent.KEYCODE_ENTER
+                )
+            )
+        }
+    }
 
+    // @SuppressLint("ResourceAsColor")
     override fun onCreateInputView(): View {
 
-         keyboardView = layoutInflater.inflate(R.layout.keyboard,null) as KeyboardView
-         keyboard= Keyboard(this,R.xml.keys)
-        keyboardView!!.apply {
-            keyboard =this@InputMethod.keyboard
-            setOnKeyboardActionListener(this@InputMethod)
-        }
-        return keyboardView!!
+//        keyboardView = layoutInflater.inflate(R.layout.keyboard, null) as KeyboardView
+//        keyboard = Keyboard(this, R.xml.keys)
+
+       val keyboardLayout = layoutInflater.inflate(R.layout.keyboard2, null)
+         keyboardView = keyboardLayout.findViewById(R.id.keyboardd) as KeyboardView
+       textView = keyboardLayout.findViewById(R.id.text_view)
+        enterImage = keyboardLayout.findViewById(R.id.imageView)
+       //keyboard= Keyboard(this,R.xml.keys)
+
+//        keyboardView!!.apply {
+//            keyboard =this@InputMethod.keyboard
+//
+//            setOnKeyboardActionListener(this@InputMethod)
+//        }
+//        keyboardView!!.keyboard = keyboard
+//        keyboardView!!.setOnKeyboardActionListener(this)
+//
+//        keyboardView!!.setBackgroundColor(getResources().getColor(R.color.whiteSmoke))
+//        return keyboardView!!
+
+        keyboard = Keyboard(this, R.xml.keys)
+        keyboardView!!.keyboard = keyboard
+        keyboardView!!.setOnKeyboardActionListener(this)
+        keyboardView!!.setBackgroundColor(getResources().getColor(R.color.whiteSmoke))
+        return keyboardLayout
     }
 
     override fun onPress(p0: Int) {
@@ -31,7 +69,7 @@ class InputMethod:InputMethodService(),KeyboardView.OnKeyboardActionListener {
     }
 
     override fun onKey(p0: Int, p1: IntArray?) {
-        val inputConnection = currentInputConnection
+         inputConnection = currentInputConnection
         if (inputConnection != null) {
             when (p0) {
                 Keyboard.KEYCODE_DELETE -> {
@@ -41,8 +79,11 @@ class InputMethod:InputMethodService(),KeyboardView.OnKeyboardActionListener {
                     } else {
                         inputConnection.commitText("", 1)
                     }
-                    caps = !caps
-                    keyboard!!.isShifted = caps
+                    if (kelime!=""){
+                        kelime = kelime.substring(0,kelime.length-1)
+                    }
+//                    caps = !caps
+//                    keyboard!!.isShifted = caps
                     keyboardView!!.invalidateAllKeys()
                 }
                 Keyboard.KEYCODE_SHIFT -> {
@@ -56,15 +97,23 @@ class InputMethod:InputMethodService(),KeyboardView.OnKeyboardActionListener {
                         KeyEvent.KEYCODE_ENTER
                     )
                 )
+
+
                 else -> {
                     var code = p0.toChar()
                     if (Character.isLetter(code) && caps) {
                         code = Character.toUpperCase(code)
                     }
                     inputConnection.commitText(code.toString(), 1)
+                    kelime+=code
+                   // textView.text=kelime
+
                 }
             }
-        }    }
+        }
+        textView.text=kelime
+
+    }
 
     override fun onText(p0: CharSequence?) {
     }
@@ -80,6 +129,7 @@ class InputMethod:InputMethodService(),KeyboardView.OnKeyboardActionListener {
 
     override fun swipeUp() {
     }
+
 
 
 }
